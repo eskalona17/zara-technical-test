d84b9b9a62fe143e49cf16ff9a314df842dd586b
eskalona17 <fer.escalonso@gmail.com>
2025-10-19 10:24:49 +0200

    add gsap library and some animations

--- src/main.js
+++ src/main.js
@@ -1,7 +1,41 @@
-import "@/styles/main.scss";
-
 // main.js
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('App cargada');
-  // inicializaciones, listeners, import dinámico de módulos si conviene
+import "@/styles/main.scss";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// ✅ Registrar el plugin
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("App cargada");
+
+  // Animación inicial (header)
+  gsap.fromTo(".container-header",
+    { opacity: 0, y: -50 },
+    { opacity: 1, y: 0, duration: 2 }
+  );
+
+  gsap.fromTo(".site-header .container-img p",
+    { opacity: 0, filter: "blur(10px)" },
+    { opacity: 1, filter: "blur(0px)", duration: 1.8, ease: "power2.out" }
+  );
+
+  gsap.utils.toArray(
+    ".third-section .container-image figure, .third-section .container-image .container-image-text"
+  ).forEach((el) => {
+    gsap.fromTo(el,
+      { opacity: 0, y: -50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.5,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: el,
+          start: "top 70%",
+          toggleActions: "play none none reverse",
+        }
+      }
+    );
+  });
 });
