928ef40414d1033e588f0c1bab6d5d7269b45c23
eskalona17 <fer.escalonso@gmail.com>
2025-10-19 21:57:31 +0200

    add animations with gsap

--- src/main.js
+++ src/main.js
@@ -2,26 +2,80 @@
 import "@/styles/main.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-// ✅ Registrar el plugin
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("App cargada");
 
-  // Animación inicial (header)
-  gsap.fromTo(".container-header",
+  gsap.fromTo(".header--content",
     { opacity: 0, y: -50 },
     { opacity: 1, y: 0, duration: 2 }
   );
 
-  gsap.fromTo(".site-header .container-img p",
+  gsap.fromTo(
+    ".section--intro .content",
     { opacity: 0, filter: "blur(10px)" },
-    { opacity: 1, filter: "blur(0px)", duration: 1.8, ease: "power2.out" }
+    {
+      opacity: 1,
+      filter: "blur(0px)",
+      duration: 1.8,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: ".section--intro",
+        start: "top 30%",
+        end: "top 10%",
+        toggleActions: "play none none reverse",
+        markers: false
+      }
+    }
   );
 
+
+  gsap.from(".carousel--secondary .carousel-text p", {
+    opacity: 0,
+    y: 30,
+    x: -20,
+    scale: 0.95,
+    stagger: 0.3,
+    duration: 1.2,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: ".carousel--secondary .carousel-text",
+      start: "top 80%",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  });
+
+  gsap.from(".section--editorial .text p", {
+    opacity: 0,
+    y: 30,
+    stagger: 0.3,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: ".section--editorial .text",
+      start: "top 80%",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  });
+
+  gsap.from(".section--closing .text p", {
+  opacity: 0,       
+  x: -50,          
+  duration: 1,
+  ease: "power2.out",
+  stagger: 0.3,    
+  scrollTrigger: {
+    trigger: ".section--closing .text",
+    start: "top 80%",  
+    toggleActions: "play none none reverse",
+    markers: false
+  }
+});
+
   gsap.utils.toArray(
-    ".third-section .container-image figure, .third-section .container-image .container-image-text"
+    ".section--story .media-text"
   ).forEach((el) => {
     gsap.fromTo(el,
       { opacity: 0, y: -50 },
@@ -38,4 +92,19 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     );
   });
+});
+
+
+gsap.from(".section--shop .shop-grid img", {
+  opacity: 0,
+  y: 20,           
+  duration: 0.8,
+  ease: "power2.out",
+  stagger: 0.1,    
+  scrollTrigger: {
+    trigger: ".section--shop .shop-grid",
+    start: "top 60%",   
+    toggleActions: "play none none reverse",
+    markers: false
+  }
 });
